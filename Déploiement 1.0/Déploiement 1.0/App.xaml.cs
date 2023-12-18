bdb9f0364a30d43c44ede836ddc9c93a0e733ef4
Guillaume Munger <1533601@etu.cchic.ca>
2023-12-18 15:29:37 -0500

    first version

---
+++ Déploiement 1.0/Déploiement 1.0/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Déploiement_1._0
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
